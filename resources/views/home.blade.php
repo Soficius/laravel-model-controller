37c22c12f9f81a8b0afcba4d4e1259e4ae4a257f
Soficius <103771506+Soficius@users.noreply.github.com>
2022-09-20 16:57:24 +0200

    fixed css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,9 +1,9 @@
 @extends('layouts.main')
 @section('content')
-    <h1>Movies</h1>
-    @foreach ($movies as $movie)
-        <div class="card-group d-flex flex-grap">
-            <div class="card p-5 ">
+    <h1 class="text-center">Movies</h1>
+    <div class="d-flex flex-wrap text-center">
+        @foreach ($movies as $movie)
+            <div class="card w-25 p-2">
                 <h1>{{ $movie->title }}</h1>
                 <div class="card-body">
                     <h5 class="card-title">{{ $movie->original_title }}</h5>
@@ -12,5 +12,6 @@
                     <p class="card-text"><small class="text-muted">{{ $movie->vote }}</small></p>
                 </div>
             </div>
-    @endforeach
+        @endforeach
+    </div>
 @endsection
